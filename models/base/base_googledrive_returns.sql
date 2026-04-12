d4fc1735ba27e7596734e95a6eee97d181a94cd3
Zirui Cao <zc2862.engineering@gmail.com>
2026-04-12 20:59:55 +0000

    Create 2 base models from Google Drive, and configure the sources.yml file.

---
+++ models/base/base_googledrive_returns.sql
@@ -0,0 +1,11 @@
+-- models/base/base_googledrive__returns.sql
+
+SELECT
+    _FILE,
+    _LINE,
+    _MODIFIED,
+    _FIVETRAN_SYNCED,
+    RETURNED_AT,
+    ORDER_ID,
+    IS_REFUNDED
+FROM {{ source('google_drive', 'returns') }}
