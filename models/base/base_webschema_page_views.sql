e9d6323ca96769f0825cf1d6146615d3281c8d1d
Liyuan SHANG <shangliyuan073@gmail.com>
2026-04-13 21:49:54 +0000

    fix some minor errors in base_item_views.sql and base_page_views.sql

--- models/base/base_webschema_page_views.sql
+++ models/base/base_webschema_page_views.sql
@@ -1,8 +1,8 @@
 SELECT
-    _fivetran_id,
-    session_id,
-    page_name,
-    CAST(view_at AS TIMESTAMP) AS view_time,
-    CAST(_fivetran_synced AS TIMESTAMP) AS _fivetran_synced,
-    CAST(_fivetran_deleted AS BOOLEAN) AS _fivetran_deleted
+    "_fivetran_id",
+    CAST("_fivetran_synced" AS TIMESTAMP) AS _fivetran_synced,
+    CAST("_fivetran_deleted" AS BOOLEAN) AS _fivetran_deleted,
+    SESSION_ID,
+    PAGE_NAME,
+    CAST(VIEW_AT AS TIMESTAMP) AS VIEW_TIME
 FROM {{ source('web_schema', 'page_views') }}
